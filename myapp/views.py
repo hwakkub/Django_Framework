725b5198ce6d524e037502fb7ee5f5fbde1371e4
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-09 01:15:51 +0700

    add path (about,form)
    link/about or link/form

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello my name is Jira</h1>")
+    return HttpResponse("<h1>หน้าแรกของเวป</h1>")
+
+def about(request):
+    return HttpResponse("<h1>เกี่ยวกับดรา</h1>")
+
+def form(request):
+    return HttpResponse("<h1>แบบฟอร์มบันทึกข้อมูล</h1")
