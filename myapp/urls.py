e7a523d86367ef845ba475ca2c0611a26da17e4b
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-10 09:23:39 +0700

    html file

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.index),
+    # path('',views.index),
+    path('', views.my_view, name='my_view'),
     path('about',views.about),
     path('form',views.form)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>หน้าแรกของเวป</h1>")
+# def index(request):
+#     return render(request, "index.html")
+
+# def about(request):
+#     return HttpResponse("<h1>เกี่ยวกับดรา</h1>")
 
 def about(request):
-    return HttpResponse("<h1>เกี่ยวกับดรา</h1>")
-
+    return render(request,'about.html')
+    
 def form(request):
-    return HttpResponse("<h1>แบบฟอร์มบันทึกข้อมูล</h1")
+    return render(request,'form.html')
+
+def my_view(request):
+    context = {}
+    return render(request,'index.html', context)
