663890ec6f17b75ff2bbf207beddfaae6faa01e5
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-10 09:29:13 +0700

    varible

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,10 @@ from django.http import HttpResponse
 # def about(request):
 #     return HttpResponse("<h1>เกี่ยวกับดรา</h1>")
 
+# def my_view(request):
+#     context = {}
+#     return render(request,'index.html', context)
+
 def about(request):
     return render(request,'about.html')
     
@@ -14,5 +18,6 @@ def form(request):
     return render(request,'form.html')
 
 def my_view(request):
-    context = {}
-    return render(request,'index.html', context)
+    name = "hwak"
+    age = 23
+    return render(request,'index.html', {"name":name,"age":age})
