fc1993d2e5b2dffb14c5ad67bfa1619dfcbcdeaf
hwakkub <152359933+hwakkub@users.noreply.github.com>
2024-01-10 15:48:17 +0700

    admin ทราบข้อมูล

--- myapp/models.py
+++ myapp/models.py
@@ -5,4 +5,7 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     date = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return "ชื่อ = "+self.name+","+str(self.name)
     
